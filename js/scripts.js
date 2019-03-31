1485de746f3c3b806319a2b49d740c2f43d8529f
zack <zackmoturi@gmail.com>
2019-03-31 19:24:31 +0300

    added a congatulatory GIF to footer

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,38 @@ $('#button').click(function() {
           event.preventDefault();
  });
 });
+
+var getGif = function() {
+  var gif = [];
+  $('img').each(function() {
+    var data = $(this).data('alt');
+    gif.push(data);
+  });
+  return gif;
+}
+var gif = getGif();
+
+// Preload all the GIF.
+var image = [];
+ 
+$.each(gif, function(index) {
+  image[index]     = new Image();
+  image[index].src = gif[index];
+});
+$('figure').on('click', function() {
+ 
+  var $this   = $(this),
+          $index  = $this.index(),
+           
+          $img    = $this.children('img'),
+          $imgSrc = $img.attr('src'),
+          $imgAlt = $img.attr('data-alt'),
+          $imgExt = $imgAlt.split('.');
+           
+  if($imgExt[1] === 'gif') {
+      $img.attr('src', $img.data('alt')).attr('data-alt', $imgSrc);
+  } else {
+      $img.attr('src', $imgAlt).attr('data-alt', $img.data('alt'));
+  }
+ 
+});
