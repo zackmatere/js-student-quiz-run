1e03dab13f7b8e53d6319a4e707691e9bfb179d8
zack <zackmoturi@gmail.com>
2019-04-01 15:46:10 +0300

    added lose GIF

--- js/scripts.js
+++ js/scripts.js
@@ -1,130 +1,32 @@
+function check() {
+    var questionOne = document.Quiz.questionOne.value;
+    var questionTwo = document.Quiz.questionTwo.value;
+    var questionThree = document.Quiz.questionThree.value;
+    var questionFour = document.Quiz.questionFour.value;
+    var questionFive = document.Quiz.questionFive.value;
+    var right = 0;
+
+
+    if (questionOne == "correct") {
+        right = right + 20;
+    };
+    if (questionTwo == "correct") {
+        right = right + 20;
+    };
+    if (questionThree == "correct") {
+        right = right + 20;
+    };
+    if (questionFour == "correct") {
+        right = right + 20;
+    };
+    if (questionFive == "correct") {
+        right = right + 20;
+    };
 
 
 
-var myQuestions = [
-    {
-        question: "What is html",
-        answers: {
-            a: 'high text markup laguage',
-            b: 'hypertext markup language',
-            c: 'none of the above'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "What is the meaning of CSS?",
-        answers: {
-            a: 'cascading style language',
-            b: 'case sensitive section',
-            c: 'cascading stylesheet'
-        },
-        correctAnswer: 'c'
-    }
-];
-{
-    question: "what is an array ? ",
-    answers: {
-        a: ' list of elements',
-        b: 'A function',
-        c: 'A cascade'
-    },
-    correctAnswer: 'a'
-},
-{
-    question: "What is What is DOM?",
-    answers: {
-        a: 'back-end logic',
-        b: 'front end logic',
-        c: 'short code '
-    },
-    correctAnswer: 'c'
-}
-];
+    document.getElementById("fifth-p").style.visibility = "visible";
+    document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
+    $(".container").fadeOut();
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-    function showQuestions(questions, quizContainer){
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for(letter in questions[i].answers){
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer){
-
-        // gather answer containers from our quiz
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
-
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-
-            // if answer is correct
-            if(userAnswer===questions[i].correctAnswer){
-                // add to the number of correct answers
-                numCorrect++;
-
-                // color the answers green
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            // if answer is wrong or blank
-            else{
-                // color the answers red
-                answerContainers[i].style.color = 'red';
-            }
-        }
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
-
-    // on submit, show results
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
-}
+};
